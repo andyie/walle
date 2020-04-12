33e1a088f2e9cc8a88dece920a66947474742558
andy <andrew.i.erickson@gmail.com>
2020-04-12 01:15:27 -0700

    updating bounce gui to more gracefully scale to different windowsss, and refine bounce logic

--- xguis/bounce.py
+++ xguis/bounce.py
@@ -10,90 +10,76 @@
  http://simpson.edu/computer-science/
 """
 
+import argparse
+import math
 import pygame
 import random
+import time
 
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-BALL_SIZE = 10
-
-
 class Ball:
     """
     Class to keep track of a ball's location and vector.
     """
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.change_x = 0
-        self.change_y = 0
+    def __init__(self, radius, screen):
+        # choose a random starting position and speed. let the maximum speed in each direction be a
+        # bounce every second.  second.
+        self._radius = radius
+        self._screen = screen
+        screen_dim = (screen.get_width(), screen.get_height())
+        self._limits = tuple((radius, d - radius) for d in screen_dim)
+        self._pos = [random.uniform(*limits) for limits in self._limits]
+        max_speed = [d for d in screen_dim]
+        self._vel = [random.uniform(-m, m) for m in max_speed]
+        self._then = None
 
+    def update(self, now):
+        # update position
+        if self._then is not None:
+            dt = now - self._then
+            for i, (p, v, limits) in enumerate(zip(self._pos, self._vel, self._limits)):
+                p += v * dt
+                if p < limits[0]:
+                    p = limits[0] + (limits[0] - p)
+                    v *= -1
+                elif p >= limits[1]:
+                    p = limits[1] + (limits[1] - p)
+                    v *= -1
+                self._pos[i] = max(limits[0], min(limits[1], p))
+                self._vel[i] = v
 
-def make_ball(ball_size, screen_width, screen_height):
-    """
-    Function to make a new, random ball.
-    """
-    ball = Ball()
-    # Starting position of the ball.
-    # Take into account the ball size so we don't spawn on the edge.
-    ball.x = random.randrange(ball_size, screen_width - ball_size)
-    ball.y = random.randrange(ball_size, screen_height - ball_size)
+        # draw the ball. need to truncate position to integers
+        pygame.draw.circle(self._screen, WHITE, [int(p) for p in self._pos], self._radius)
 
-    # Speed and direction of rectangle
-    max_speed_dim = max(screen_width // 50, 1)
-    ball.change_x = random.randrange(-max_speed_dim, max_speed_dim)
-    ball.change_y = random.randrange(-max_speed_dim, max_speed_dim)
-
-    return ball
-
+        self._then = now
 
 def main():
-    """
-    This is our main program.
-    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_balls', type=int, default=1)
+    args = parser.parse_args()
+    assert args.num_balls > 0
+
     pygame.init()
     pygame.display.set_caption("Bouncing Balls")
     screen = pygame.display.set_mode((0, 0))
     clock = pygame.time.Clock()
-
-    ball_list = []
-    ball_size = screen.get_width() // 10
-    ball = make_ball(ball_size, screen.get_width(), screen.get_height())
-    ball_list.append(ball)
+    screen_dim = (screen.get_width(), screen.get_height())
+    ball_radius = screen.get_width() // 20 # let division truncate to 0
+    print('Using screen {}x{} and ball radius {}'.format(*screen_dim, ball_radius))
+    balls = [Ball(ball_radius, screen) for _ in range(args.num_balls)]
 
     done = False
     while not done:
-        # --- Logic
-        for ball in ball_list:
-            # Move the ball's center
-            ball.x += ball.change_x
-            ball.y += ball.change_y
-
-            # Bounce the ball if needed
-            if ball.y > screen.get_height() - ball_size or ball.y < ball_size:
-                ball.change_y *= -1
-            if ball.x > screen.get_width() - ball_size or ball.x < ball_size:
-                ball.change_x *= -1
-
-        # --- Drawing
-        # Set the screen background
+        now = time.time()
         screen.fill(BLACK)
-
-        # Draw the balls
-        for ball in ball_list:
-            #pygame.draw.line(screen, WHITE, [ball.x, ball.y], [ball.x, ball.y], 1)
-            pygame.draw.circle(screen, WHITE, [ball.x, ball.y], ball_size)
-
-        # --- Wrap-up
-        # Limit to 60 frames per second
+        for ball in balls:
+            ball.update(now)
         clock.tick(60)
-
-        # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
 
-    # Close everything down
     pygame.quit()
 
 if __name__ == "__main__":
