406f44590790894e831bda77fa6d22f572805407
Ben Wasserman <quantumcube@gmail.com>
2020-06-13 16:19:56 -0700

    Fixs to images

--- image.py
+++ image.py
@@ -11,29 +11,32 @@ from PIL import Image, ImageSequence
 import walle
 
 
+def _r(pixel):
+    return tuple([subpixel/255. for subpixel in pixel])
+
+
 class ImageDisplay:
     def __init__(self, driver, path):
         self.driver = driver
         self.image = Image.open(path)
         self.frames = []
+        self.frame_idx = 0
 
         # Prepare frames
         orig_height, orig_width = self.image.size
         disp_height, disp_width = driver.dim()
         factor_height = orig_height // disp_height + 1
         factor_width = orig_width // disp_width + 1
-        self.image = self.image.reduce((factor_height, factor_width))
                 
         if self.image.is_animated:
             for frame in ImageSequence.Iterator(self.image):
-                self.frames.append([[frame.convert('RGB').get_pixel((x, y)) for x in disp_width] for y in disp_height])
+                self.frames.append([[_r(frame.convert('RGB').reduce((factor_height, factor_width)).getpixel((x, y))) for x in range(disp_width)] for y in range(disp_height)])
         else:
-            self.frames.append([[self.image.convert('RGB').get_pixel((x, y)) for x in disp_width] for y in disp_height])
+            self.frames.append([[_r(self.image.convert('RGB').reduce((factor_height, factor_width)).getpixel((x, y))) for x in range(disp_width)] for y in range(disp_height)])
 
     def update(self):
-        while True:
-            for frame in self.frames:
-                self.driver.set(frame)
+        self.driver.set(self.frames[self.frame_idx % len(self.frames)])
+        self.frame_idx += 1
 
 
 if __name__ == '__main__':
